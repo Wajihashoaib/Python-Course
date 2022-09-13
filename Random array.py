ac275440178004ee0aece478dd14ea6f556aec33
Wajiha Shoaib <81462958+Wajihashoaib@users.noreply.github.com>
2022-09-13 15:17:12 +0500

    Random array (integer) using numpy

---
+++ Random array.py
@@ -0,0 +1,12 @@
+#Generation of Random array using numpy
+
+#Importing numpy libraray
+import numpy as np 
+
+
+#function for Generating random(random.randint)
+#number between the range of 1and 5.
+
+
+array = np.random.randint(1, 5, size=(4, 4))
+print(array)

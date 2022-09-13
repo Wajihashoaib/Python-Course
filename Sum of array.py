a8eef3f777933a7fe09fc909af7e40f4db49332e
Wajiha Shoaib <81462958+Wajihashoaib@users.noreply.github.com>
2022-09-13 15:13:19 +0500

    Sum of 1D array with or without Numpy

---
+++ Sum of array.py
@@ -0,0 +1,23 @@
+#Sum of array with or without numpy
+
+#without numpy
+X=[1,2,3]
+Y=[5,6,7]
+
+
+#Sum
+X+Y
+
+#Importing numpy libraray
+import numpy as np 
+
+#for to check version of numpy
+
+numpy.__version__
+
+#declaration of two 1D array
+X1=np.array([1,2,3])
+Y1=np.array([5,6,7])
+
+#Sum of the array
+X1+Y1
